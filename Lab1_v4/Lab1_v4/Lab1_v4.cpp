ea12d6ac0595dbf203f58c406d3352196ea2e5a5
Andrii Vashchuk <48751874+AndriiVashchuk@users.noreply.github.com>
2020-04-02 11:44:44 +0300

    Add files via upload

--- Lab1_v4/Lab1_v4/Lab1_v4.cpp
+++ Lab1_v4/Lab1_v4/Lab1_v4.cpp
@@ -110,14 +110,6 @@ void Union(struct subset subsets[], int x, int y)
 }
 int main()
 {
-	/* Let us create following weighted graph
-			10
-		0--------1
-		| \	     |
-	   6|  5\    |15
-		|	  \  |
-		2--------3
-			4	 */
 	int V = 4; // Number of vertices in graph 
 	int E = 5; // Number of edges in graph 
 	struct Graph* graph = createGraph(V, E);
@@ -125,27 +117,27 @@ int main()
 	// add edge 0-1 
 	graph->edge[0].src = 0;
 	graph->edge[0].dest = 1;
-	graph->edge[0].weight = 10;
+	graph->edge[0].weight = 8;
 
 	// add edge 0-2 
 	graph->edge[1].src = 0;
 	graph->edge[1].dest = 2;
-	graph->edge[1].weight = 6;
+	graph->edge[1].weight = 3;
 
 	// add edge 0-3 
 	graph->edge[2].src = 0;
 	graph->edge[2].dest = 3;
-	graph->edge[2].weight = 5;
+	graph->edge[2].weight = 4;
 
 	// add edge 1-3 
 	graph->edge[3].src = 1;
 	graph->edge[3].dest = 3;
-	graph->edge[3].weight = 15;
+	graph->edge[3].weight = 12;
 
 	// add edge 2-3 
 	graph->edge[4].src = 2;
 	graph->edge[4].dest = 3;
-	graph->edge[4].weight = 4;
+	graph->edge[4].weight = 5;
 
 	boruvkaMST(graph);
 
